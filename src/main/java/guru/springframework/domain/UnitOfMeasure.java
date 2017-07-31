176ffee3c7f09303086141d27d7ffc6efe3b571a
rhye <gacillos.rhye@gmail.com>
2017-07-31 17:27:19 -0400

    added command object and type conversions

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -7,9 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- * Created by jt on 6/13/17.
- */
 @Data
 @Entity
 public class UnitOfMeasure {
@@ -17,5 +14,7 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
+
 }
