35d3288a7a4e669f4724ea184b69a1b72a77d420
rhye <gacillos.rhye@gmail.com>
2017-08-25 02:11:08 +0800

    added spring repository - CategoryRepository - RecipeRepository - UnitOfMeasureRepository

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -10,7 +10,6 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     private String description;
     private Integer prepTime;
     private Integer cookTime;
@@ -18,7 +17,7 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
+
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
@@ -30,6 +29,12 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -125,5 +130,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
